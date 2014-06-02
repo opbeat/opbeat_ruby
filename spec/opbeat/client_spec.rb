7af4044eea86bed52db77980cde6f3084c055a71
Ron Cohen <ron@opbeat.com>
2014-06-02 20:06:36 +0200

    Slight tests cleanup.

--- spec/opbeat/client_spec.rb
+++ spec/opbeat/client_spec.rb
@@ -9,13 +9,7 @@ describe Opbeat::Client do
     @configuration.current_environment = :test
     @client = Opbeat::Client.new(@configuration)
     @client.stub(:send)
-    # @client.stub(:send) { "bleh" }
-
-  #   @event = double("event")
-  #   Opbeat.stub(:send) { @send }
-      
-  #   Opbeat::Event.stub(:capture_exception) { @event }
-  end
+   end
 
   it 'send_release should send' do
     message = "Test message"
@@ -28,14 +22,4 @@ describe Opbeat::Client do
     @client.should_receive(:send).with("/errors/", event)
     @client.send_event(event)
   end
-
-
-  # it 'send_release should send' do
-  #   exception = build_exception()
-
-  #   Opbeat::Event.should_receive(:capture_exception).with(exception)
-  #   Opbeat.should_receive(:send).with(@event)
-
-  #   Opbeat.captureException(exception)
-  # end
 end
