a1a19fe01da704ced3d16991b62e36eb99b50c6f
Miguel Palhas <mpalhas@gmail.com>
2014-12-09 12:39:06 +0000

    Fixes error condition

--- lib/opbeat/capistrano/capistrano3.rb
+++ lib/opbeat/capistrano/capistrano3.rb
@@ -18,28 +18,6 @@ namespace :opbeat do
         end
       end
     end
-
-      # within repo_path do
-      #   rev = fetch(:current_revision)
-
-      #   branches = capture("cd #{repo_path}; /usr/bin/env git branch --contains #{rev}").split
-      #   if branches.length == 1
-      #     branch = branches[0].sub("* ", "")
-      #   else
-      #     branch = nil
-      #   end
-
-      #   notify_command = "REV=#{rev} "
-      #   notify_command << "BRANCH=#{branch} " if branch
-
-      #   rails_env = fetch(:rails_env, "production")
-      #   notify_command << "RAILS_ENV=#{rails_env} "
-
-      #   executable = fetch(:rake, 'bundle exec rake ')
-      #   notify_command << "#{executable} opbeat:deployment"
-      #   capture ("cd #{release_path};" + notify_command), :once => true
-      # end
-    # end
   end
 end
 
